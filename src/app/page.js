b706d000b2bfa3ba2c70e33a970a8812acdd8758
Frederick Valle <freddy.gomez.245@gmail.com>
2025-03-07 21:37:30 -0500

    second commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,32 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { useChat } from "@ai-sdk/react";
+
+export default function Chat() {
+  const { messages, input, handleInputChange, handleSubmit } = useChat({
+    maxSteps: 5,
+  });
   return (
-    <main className="grid place-content-center h-screen">
-      First AI app
-    </main>
+    <>
+      <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch h-auto min-h-[10vh]">
+        {messages.map((m) => (
+          <div key={m.id} className="whitespace-pre-wrap overflow-y-auto max-h-[80vh]">
+            {m.role === "user" ? "You: " : "AI: "} {m.content}
+          </div>
+        ))}
+
+        <form onSubmit={handleSubmit}>
+          <input
+            className="fixed dark:bg-zinc-900 bottom-5 w-full max-w-md p-2 mb-8 border border-zinc-300 dark:border-zinc-800 rounded shadow-xl"
+            value={input}
+            placeholder="Say something..."
+            onChange={handleInputChange}
+          />
+        </form>
+      </div>
+      <div>
+        <footer className="fixed bottom-5 px-5">Client developed by Frederick Valle</footer>
+      </div>
+    </>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+export default {
+  darkMode: "class", // Enable dark mode using the class strategy
+  theme: {
+    extend: {}, // You can extend the theme here if needed
+  },
+};
